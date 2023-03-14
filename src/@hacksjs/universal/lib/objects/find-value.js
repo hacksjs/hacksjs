c2d7061fdb97083b697c5b4ee748fee27b6475fc
Hendrik Michiels <hendrik.michiels77@gmail.com>
2023-03-14 16:42:54 +0000

    feature: add universal/objects/findValue function

---
+++ src/@hacksjs/universal/lib/objects/find-value.js
@@ -0,0 +1,53 @@
+// @ts-check
+
+/**
+ * @copyright Hacks Ltd
+ * @license   MIT
+ */
+
+/**
+ * Find a value, deeply nested within an object structure.
+ *
+ * This function provides a convenient way to access nested properties without needing to accommodate the possibility of
+ * in-between properties being undefined. This is now possible using modern ES syntax, a combination of optional chaining
+ * (`?.`) and the nullish coalescing operator (`??`) to provide a fallback.
+ *
+ * @since 1.0.0
+ * @category objects
+ *
+ * @param {Object} object The object to query.
+ * @param {string|string[]} path Array or dot-noted string representing the path to a nested own property of `object`.
+ * @param {*} [fallback] Default value if path cannot be resolved.
+ * @returns {*} The resolved value, else the `fallback` value.
+ *
+ * @example
+ *
+ * const target = { 'a': [{ 'b': { 'c': 100 } }] }
+ *
+ * findValue(target, 'a[0].b.c')
+ * // → 100
+ *
+ * findValue(target, ['a', '0', 'b', 'c'])
+ * // → 100
+ *
+ * // Equivalent to:
+ * target?.a[0]?.b?.c ?? null
+ */
+const findValue = (object, path, fallback) => {
+  if (typeof path === 'string') {
+    path = path.split(/[.\[\]\"]+/).filter(x => x)
+  }
+
+  if (path.length === 0) {
+    return fallback
+  }
+
+  const [head, ...tail] = path
+  if (!(head in object)) {
+    return fallback
+  }
+
+  return findValue(object[head], tail, fallback)
+}
+
+export { findValue }
--- src/@hacksjs/universal/lib/objects/index.js
+++ src/@hacksjs/universal/lib/objects/index.js
@@ -5,4 +5,5 @@
  * @license   MIT
  */
 
+export { findValue } from './find-value'
 export { objectToString, object_tostring_map } from './object-to-string'
